9eec235c8eafb135724c3b2fccf9dbc4ba12ddf6
王伟兵 <javacup00@163.com>
2016-08-08 07:53:09 +0800

    Update imaicloud_token.sh

--- srv/imaicloud_token.sh
+++ srv/imaicloud_token.sh
@@ -5,7 +5,8 @@
 # description: generate imaicloud token
 # param: imaicloud_expires - 过期时间（自1970年的毫秒数）
 # param: imaicloud_payload - 载荷，是base64url编码的字符串
-# maicloud_role - 角色，如果多个则用"|"隔开
+# param: imaicloud_role - 角色，如果多个则用"|"隔开
+# param: imaicloud_userid - 用户id，即租户在imaicloud.com的二级域名
 # filtered_param: pwd - provent print password to log
 # -- jojo --
 
